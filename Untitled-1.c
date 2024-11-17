02ac8b29f6c8613ead33c414c3cd37591c10f489
sai mohan <110048126+sai8328-l@users.noreply.github.com>
2024-11-17 13:11:59 +0530

    Add files via upload

---
+++ Untitled-1.c
@@ -0,0 +1,23 @@
+<script>
+	int a = 5;
+	int b = 5;
+
+	// PREFIX
+	int prefix = ++a;
+	printf("Prefix Increment: %d\n", prefix);
+
+	// POSTFIX
+	int postfix = b++;
+	printf("Postfix Increment: %d", postfix);
+  let c=b
+    printf(c);
+}
+
+// Driver code
+int main()
+{
+	increment();
+
+	return 0;
+}
+
